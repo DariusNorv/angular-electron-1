5ddb9aa1cef3853c2e2f43dcd38a099390279c12
Konstantin Vulsonov <djnafany@gmail.com>
2017-08-23 02:10:54 +0300

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
 import { HomeComponent } from './components/home/home.component';
+import { SettingsComponent } from './components/settings/settings.component'
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {useHash: true})],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
