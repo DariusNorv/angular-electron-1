69d2712296b03dbecced36d35ab5c8bd956f19be
Konstantin Vulsonov <djnafany@gmail.com>
2017-09-05 01:10:11 +0300

    mixcloud work play/pause... it's a bit of shame

--- main.ts
+++ main.ts
@@ -6,8 +6,7 @@ const args = process.argv.slice(1);
 serve = args.some(val => val === '--serve');
 
 if (serve) {
-  require('electron-reload')(__dirname, {
-  });
+  require('electron-reload')(__dirname, {});
 }
 
 function createWindow(): void {
@@ -28,8 +27,11 @@ function createWindow(): void {
 
   // Open the DevTools.
   if (serve) {
+    // const extension = '/Users/kostya.v/Library/Application\\ Support/Google/' +
+    //   'Chrome/Default/Extensions/elgalmkoelokbchhkhacckoklkejnhcd/1.14.0_0';
+    // BrowserWindow
+    //   .addDevToolsExtension(path.resolve(extension));
     win.webContents.openDevTools();
-    // win.webContents.addDevToolsExtension('~/Library/Application\\ Support/Google/Chrome/Default/Extensions/');
   }
 
   // Emitted when the window is closed.
---
+++ src/app/components/navbar-player/navbar-player.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { PlayerService } from '../../providers/player.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { isDefined } from '@angular/compiler/src/util';
+
+@Component({
+  selector: 'app-navbar-player',
+  templateUrl: './navbar-player.component.html',
+  styleUrls: ['./navbar-player.component.scss'],
+})
+export class NavbarPlayerComponent implements OnInit {
+  private url;
+  private isplaying: boolean;
+  private previous: string;
+
+  constructor(private pl: PlayerService, private sanitizer: DomSanitizer) {
+  }
+
+  protected startPlayer(): void {
+    // if (this.url.changingThisBreaksApplicationSecurity.length > 0) {
+    // Stop before play
+    // if (this.pl.getMixcloudPlayerButton('mixCloudIframe', 'pause')) {
+    //   this.pl.getMixcloudPlayerButton('mixCloudIframe', 'pause').click();
+    // };
+    // this.pl.getMixcloudPlayerButton('mixCloudIframe', 'play').click();
+    // this.mixCloudPlaying = !this.mixCloudPlaying;
+    if (!this.isplaying) {
+      this.pl.getMixcloudPlayerButton('mixCloudIframe', 'play').click();
+      this.isplaying = true;
+    }
+    console.log('loaded');
+    // }
+  }
+
+  ngOnInit() {
+    this.isplaying = false;
+    this.pl.url.subscribe((url) => {
+      // debugger;
+      // Do we need to update url
+      if (this.previous !== url) {
+        // Stop playing before update
+        if (this.isplaying && url !== '') {
+          this.pl.getMixcloudPlayerButton('mixCloudIframe', 'pause').click();
+          this.isplaying = false;
+        }
+        this.url = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+      } else {
+        if (this.isplaying) {
+          this.pl.getMixcloudPlayerButton('mixCloudIframe', 'pause').click();
+          this.isplaying = false;
+        } else {
+          this.pl.getMixcloudPlayerButton('mixCloudIframe', 'play').click();
+          this.isplaying = true;
+        }
+      }
+      this.previous = url;
+    })
+  }
+}
